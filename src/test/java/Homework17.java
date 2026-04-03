dc18d203ca9c6cd3841bc237775d06812cb81b5a
AnitaAnita1010 <TinaTina4040@@>
2026-04-03 18:24:04 +0100

    Homework17
    Updated Homework17

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -22,7 +23,7 @@ public class Homework17 extends BaseTest {
 
         WebElement emailField = driver.findElement(By.xpath("//input[@type='email']"));
         emailField.clear();
-        emailField.sendKeys("anita.surewicz@testpro.io=");
+        emailField.sendKeys("anita.surewicz@testpro.io");
 
         WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
         passwordField.clear();
@@ -37,7 +38,7 @@ public class Homework17 extends BaseTest {
         WebElement viewAllBtn = driver.findElement(By.xpath("//button[@data-test='view-all-songs-btn']"));
         viewAllBtn.click();
 
-        WebElement firstSong = driver.findElement(By.xpath ("//tr[@class='song-item'][1]"));
+        WebElement firstSong = driver.findElement(By.xpath("//tr[@class='song-item'][1]"));
         firstSong.click();
 
         WebElement addToBtn = driver.findElement(By.xpath("//button[@class='btn-add-to']"));
@@ -46,13 +47,17 @@ public class Homework17 extends BaseTest {
         WebElement homework17Playlist = driver.findElement(By.xpath("//*[@id=\"songsWrapper\"]/header/div[3]/div/section[1]/ul/li[5]"));
         homework17Playlist.click();
 
-        WebElement notification = driver.findElement(By.xpath("////div[@class='success show']]"));
-        }
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-        
+        WebElement notification = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='success show']"))
+        );
+
+        String actualMessage = notification.getText().trim();
+        String expectedSongAddedMessage = "Added 1 song into \"Homework17\"";
+
+        Assert.assertEquals(actualMessage, expectedSongAddedMessage);
 
 
-
-
-
+    }
 }
