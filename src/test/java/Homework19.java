00a037b3e65a802e7ef7f6dbb567e3e16fba3cc8
AnitaAnita1010 <TinaTina4040@@>
2026-04-07 15:24:25 +0100

    .
    .

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,2 +1,10 @@
-public class Homework19 {
+import org.testng.annotations.Test;
+
+@Test
+    public void playSong() throws InterruptedException {
+        navigateToPage();
+        provideEmail("anita.surewicz@testpro.io");
+        providePassword("AnitaAnita1029");
+        clickLoginButton();
+        Thread.sleep(3000);
 }
