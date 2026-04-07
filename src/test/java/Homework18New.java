d811343eb011a4cac7480b8b9b9170d02fcf69ba
AnitaAnita1010 <TinaTina4040@@>
2026-04-07 15:09:22 +0100

    commit
    commit

--- src/test/java/Homework18New.java
+++ src/test/java/Homework18New.java
@@ -1,3 +1,6 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework18New extends BaseTest {
@@ -8,7 +11,48 @@ public class Homework18New extends BaseTest {
             provideEmail("anita.surewicz@testpro.io");
             providePassword("AnitaAnita1029");
             clickLoginButton();
+            clickPlay();
             Thread.sleep(3000);
+
+            Assert.assertTrue(isSongPlaying());
+    }
+
+    public void clickPlay() {
+        WebElement playNextBtn = driver.findElement(By.xpath("//i[@role='button' and @title='Play next song']"));
+        WebElement playBtn = driver.findElement(By.xpath("//span[@role='button' and @title='Play or resume']"));
+        playNextBtn.click();
+        playBtn.click();
+    }
+
+    public boolean isSongPlaying() {
+        WebElement soundBar = driver.findElement(By.xpath("//img[@alt='Sound bars']"));
+        return soundBar.isDisplayed();
     }
 }
+
+/* clickPlayNextSong();
+        clickPlayBtn();
+        soundBarVisible();
+    }
+
+    public void clickPlayNextSong() throws InterruptedException {
+        WebElement playNext = driver.findElement(By.xpath("//i[@role='button' and @title='Play next song']"));
+        playNext.click();
+        Thread.sleep(3000);
+    }
+
+    public void clickPlayBtn() throws InterruptedException {
+        WebElement clickPlay = driver.findElement(By.xpath("//span[@role='button' and @title='Play or resume']"));
+        clickPlay.click();
+        Thread.sleep(3000);
+    }
+
+    public void soundBarVisible() {
+        WebElement soundBar = driver.findElement(By.xpath("//img[@alt='Sound bars']"));
+        Assert.assertTrue(soundBar.isDisplayed(), "If sound bar is visible, song is playing");
+    }
+
+
+}
+*/
 
