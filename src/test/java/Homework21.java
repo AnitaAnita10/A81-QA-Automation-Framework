c28ea46e6ca9307d8f2a0db4db3b8d8140cf390f
AnitaAnita10 <anita.surewicz@testpro.io>
2026-04-10 14:21:11 +0100

    .
    .

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -1,2 +1,42 @@
-public class Homework21 { ...
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework21 extends BaseTest {
+
+    String newPlaylistName = "Anita Again";
+
+    @Test
+    public void renamePlayList() {
+
+        String updatedPlayListMsg = "Updated playlist \"Anita Again.\"";
+
+        /*navigateToPage();*/
+        provideEmail("anita.surewicz@testpro.io");
+        providePassword("AnitaAnita1029");
+        clickLoginButton();
+        doubleClickPlayList();
+        enterNewPlayListName();
+        Assert.assertEquals(getRenamePlayListSuccessMsg(),updatedPlayListMsg);
+
+    }
 }
+
+
+
+/*public class Homework20 extends BaseTest{
+
+    @Test
+    public void deletePlayList() {
+
+        String ExpectedSongDeletedMessage = "Deleted playlist \"Homework19.\"";
+
+        navigateToPage();
+        provideEmail("anita.surewicz@testpro.io");
+        providePassword("AnitaAnita1029");
+        clickLoginButton();
+
+        clickPlayList();
+        clickDeletePlayList();
+
+        Assert.assertEquals(getSongDeletedMessage(), ExpectedSongDeletedMessage);
+    }*/
