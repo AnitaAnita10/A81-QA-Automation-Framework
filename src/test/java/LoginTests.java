38a73c5f01676753f9e6e3b50a7194cd703f34c6
AnitaAnita10 <anita.surewicz@testpro.io>
2026-04-13 13:32:22 +0100

    .
    .

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,27 +1,15 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
+import Pages.LoginPage;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class LoginTests extends BaseTest {
+
     @Test
-    public void loginEmptyEmailPassword() {
-
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        // TODO (for students): Review the configuration as part of HW15
-        
-        String url = "httpps://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+    public void loginValidEmailPassword() {
+        LoginPage = loginPage = new LoginPage(driver);
     }
+
+    loginPage.provideEmail("anita.surewicz@testpro.io");
+    loginPage.providePassword("AnitaAnita1029");
+    loginPage.clickSubmit();
+}
 }
