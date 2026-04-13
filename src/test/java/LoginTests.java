e157ac8a6c1b639afa9d981ac407d45108e4e2f2
AnitaAnita10 <anita.surewicz@testpro.io>
2026-04-13 13:37:37 +0100

    .
    .

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -5,8 +5,7 @@ public class LoginTests extends BaseTest {
 
     @Test
     public void loginValidEmailPassword() {
-        LoginPage = loginPage = new LoginPage(driver);
-    }
+        LoginPage loginPage = new LoginPage(driver);
 
     loginPage.provideEmail("anita.surewicz@testpro.io");
     loginPage.providePassword("AnitaAnita1029");
