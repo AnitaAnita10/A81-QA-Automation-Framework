29dbe59354684afe5731ac8c812aafc19bdd6bde
AnitaAnita10 <anita.surewicz@testpro.io>
2026-04-13 13:50:09 +0100

    dfd
    dsfas

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import Pages.HomePage;
 import Pages.LoginPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends BaseTest {
@@ -6,9 +8,12 @@ public class LoginTests extends BaseTest {
     @Test
     public void loginValidEmailPassword() {
         LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
 
     loginPage.provideEmail("anita.surewicz@testpro.io");
     loginPage.providePassword("AnitaAnita1029");
     loginPage.clickSubmit();
+
+        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
 }
 }
