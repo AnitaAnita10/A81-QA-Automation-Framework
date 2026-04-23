7099f482e55c7162b78e00b6db27f7bd3fcb9491
AnitaAnita10 <anita.surewicz@testpro.io>
2026-04-23 13:39:25 +0100

    f

--- src/test/java/StepDefinitions/LoginStepDefinitions.java
+++ src/test/java/StepDefinitions/LoginStepDefinitions.java
@@ -1,4 +1,14 @@
 package StepDefinitions;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class LoginStepDefinitions {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @Given("")
+    public void openBrowser() {
+
+    }
 }
