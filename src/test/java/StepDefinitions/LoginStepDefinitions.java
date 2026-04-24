bbfe4919a0e21d437d842ce2c75501cc69c6c515
AnitaAnita10 <anita.surewicz@testpro.io>
2026-04-24 13:43:07 +0100

    f

--- src/test/java/StepDefinitions/LoginStepDefinitions.java
+++ src/test/java/StepDefinitions/LoginStepDefinitions.java
@@ -6,10 +6,13 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
@@ -29,21 +32,31 @@ public class LoginStepDefinitions {
 
     @And("I am in the Koel login page")
     public void iAmInTheKoelLoginPage() {
+        driver.get("https://qa.koel.app/#!/home");
     }
 
     @When("I input email {string}")
-    public void iInputEmail(String arg0) {
+    public void iInputEmail(String email) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("[type='email']"))).sendKeys(email);
     }
 
     @When("I input password {string}")
-    public void iInputPassword(String arg0) {
+    public void iInputPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("[type='password']"))).sendKeys(password);
     }
 
     @And("I click Log in")
     public void iClickLogIn() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("[type='submit']"))).click();
     }
 
     @Then("I am logged in")
     public void iAmLoggedIn() {
+        Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("img.avatar"))).isDisplayed());
+
     }
 }
