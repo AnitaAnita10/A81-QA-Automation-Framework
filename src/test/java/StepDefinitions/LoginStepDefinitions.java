1fb27c1f80697191624ba75e6492e7a170e40246
AnitaAnita10 <anita.surewicz@testpro.io>
2026-04-24 13:31:19 +0100

    k

--- src/test/java/StepDefinitions/LoginStepDefinitions.java
+++ src/test/java/StepDefinitions/LoginStepDefinitions.java
@@ -1,6 +1,10 @@
 package StepDefinitions;
 
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,13 +17,33 @@ public class LoginStepDefinitions {
     WebDriver driver;
     WebDriverWait wait;
 
-    @Before("I open the browser")
+    @Given("I open the browser")
     public void openBrowser() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifactions");
-        options.addArguments("--remote-allow-origins*");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @And("I am in the Koel login page")
+    public void iAmInTheKoelLoginPage() {
+    }
+
+    @When("I input email {string}")
+    public void iInputEmail(String arg0) {
+    }
+
+    @When("I input password {string}")
+    public void iInputPassword(String arg0) {
+    }
+
+    @And("I click Log in")
+    public void iClickLogIn() {
+    }
+
+    @Then("I am logged in")
+    public void iAmLoggedIn() {
     }
 }
