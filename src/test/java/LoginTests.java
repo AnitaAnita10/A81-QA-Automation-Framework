bd4c327e721c5018360730e30396adc8ae7a2543
AnitaAnita10 <anita.surewicz@testpro.io>
2026-04-24 15:13:00 +0100

    .

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -19,9 +19,32 @@ public class LoginTests extends BaseTest {
     public void loginEmptyEmailPassword() throws InterruptedException {
 
         LoginPage loginPage = new LoginPage(getDriver());
-        loginPage.provideEmail("").providePassword("dfdsafasd").clickSubmit();
+        loginPage.provideEmail("").providePassword("").clickSubmit();
 
         Assert.assertEquals(getDriver().getCurrentUrl(), url);
+    }
+
+    @Test
+    public void loginInvalidEmailValidPassword() {
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+
+        loginPage.provideEmail("dfaf@gmail.com")
+                .providePassword("AnitaAnita1029")
+                .clickSubmit();
+
+        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+    }
+
+    @Test
+    public void loginValidEmailEmptyPassword() {
+        LoginPage loginPage = new LoginPage(getDriver());
+
+        loginPage.provideEmail("anita.surewicz@testpro.io")
+                .providePassword("")
+                .clickSubmit();
+
+        Assert.assertEquals(getDriver().getCurrentUrl(), "https://qa.koel.app/");
     }
 
 
